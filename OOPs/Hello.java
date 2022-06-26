25fe468f6c66b9d96a5a63291641bd25c4dec7e7
Kpatel1104 <107944333+Kpatel1104@users.noreply.github.com>
2022-06-26 19:33:38 +0530

    Add files via upload

---
+++ OOPs/Hello.java
@@ -0,0 +1,22 @@
+
+abstract class Hello {
+
+	static void demofun()
+	{
+
+		
+		System.out.println("Hello World");
+	}
+}
+
+
+public class HW extends Hellos{
+
+	
+	public static void main(String[] args)
+	{
+
+		
+		Hello.demofun();
+	}
+}
